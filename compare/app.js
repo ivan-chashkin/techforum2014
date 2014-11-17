41c5199e14c4d4d849cc0f9cb3c8bc398de7fb2d
Ivan Chashkin <i.chashkin@mail.ru>
2014-11-18 00:13:46 +0300

    + connection close xhr;

--- compare/app.js
+++ compare/app.js
@@ -1,3 +1,6 @@
+'use strict';
+/* global require, __dirname, console */
+
 var 
 	http = require('http'),
 	serveStatic = require('serve-static'),
@@ -6,23 +9,26 @@ var
 	port = 23429
 ;
 
-var serve = serveStatic(__dirname + base_folder)
+var serve = serveStatic(__dirname + base_folder);
 
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res);
   serve(req, res, done);
-})
+});
 
 server.listen(port);
 
 var io = require('socket.io').listen(server);
 var count = 0;
 io.sockets.on('connection', function (socket) {
+
 	socket.emit('test');
-	socket.on('test', function (data) {
-		socket.emit('test', {test: 'test '+count});
+
+	socket.on('test', function () {
+		socket.emit('test', { test: 'test ' + count });
 		count++;
 	});
+
 });
 
 console.log('Listen port: ' + port);
