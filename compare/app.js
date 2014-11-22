b394d1973d175efb1aabcea81a1fc8ce5737aa66
Ivan Chashkin <i.chashkin@mail.ru>
2014-11-22 17:17:07 +0300

    websocket native test

--- compare/app.js
+++ compare/app.js
@@ -13,39 +13,42 @@ var serve = serveStatic(__dirname + base_folder);
 
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res);
-  console.log(req.url);
   if (req.url.match('test_close.html')) {
   	res.setHeader('Connection', 'close');
   }
-  serve(req, res, done);
+  //serve(req, res, done);
+});
+
+var WebSocketServer = require('websocket').server;
+var wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: true
+});
+var res = [];
+for(var i in wsServer){
+	res.push(i);
+}
+console.log(res.join(', '));
+wsServer.on('message', function(){
+	console.log('wtf');
+});
+wsServer.addListener('request', function(request) {
+	console.log('request');
+
+    request.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            request.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            request.sendBytes(message.binaryData);
+        }
+    });
+    request.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
 
 server.listen(port);
-
-var io = require('socket.io').listen(server);
-
-var count = 0;
-io.sockets.on('connection', function (socket) {
-	console.log('connection');
-
-	socket.emit('test');
-
-	socket.on('message', function (data) {
-		console.log('message: ' + data);
-	});
-
-	socket.on('all', function () {
-		console.log('all: ' + arguments);
-	});
-
-	socket.on('test', function (data) {
-		try {
-			console.log('test: ' + JSON.stringify(data));
-		} catch (ex) {}
-		socket.emit('test', { test: 'test ' + count });
-		count++;
-	});
-
-});
-
 console.log('Listen port: ' + port);
